82e45cd14930e7424508b98ddcbc67994fab3dbe
Hever Farber <hever.farber@gmail.com>
2023-08-31 10:00:04 +0300

    first-script

--- modules-detection.mjs
+++ modules-detection.mjs
@@ -1,11 +1,19 @@
 #!/usr/bin/env zx
 
-await $`ls`
-await $`printenv`
+const { ENV0_PR_SOURCE_BRANCH, ENV0_PR_TARGET_BRANCH} = process.env;
 
+const sourceRef = ENV0_PR_SOURCE_BRANCH;
+const targetRef = ENV0_PR_TARGET_BRANCH;
 
-if (process.env.ENV0_DEPLOYMENT_TYPE === 'deploy') {
-    console.log('I am here');
-} else {
-    console.log('Not here');
+await detect();
+
+async function detect() {
+    if (!sourceRef || !targetRef) {
+        console.log(`skipping - can't detect source or target refs. source [${sourceRef}] target [${targetRef}]`);
+        return;
+    }
+
+    const { stdout: fileChangesRaw } = await $`git diff --name-only  ${sourceRef}..${targetRef}`;
+
+    console.log(fileChangesRaw.trim().split('\n'));
 }
